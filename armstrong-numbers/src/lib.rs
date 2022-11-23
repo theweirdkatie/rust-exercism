78e550ab09988ace1deaa83cc57a22d78bb379c8
Katherine <kstedman@thehouseofdesign.com>
2022-11-22 17:00:52 -0700

    final

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let mut numbers: Vec<u32> = vec![];
+    let mut _sum = 0;
+    
+    // convert the number to a string
+    // iterator over characters
+    // for each character, convert back to number, push onto number vector
+    num.to_string().chars().for_each( |x| numbers.push(x.to_digit(10).unwrap_or_default()));
+    
+    // iterate over number vector
+    // raise each digit to the numbers of digits and add to sum
+    numbers.iter().for_each( |x| _sum += x.pow(numbers.len().try_into().unwrap()));
+    
+    // if sum == original number, is armstrong number
+    _sum == num
 }

de5ee7fa9e9c1cfa458aa829c5b84094d7b00d1b
Katherine <kstedman@thehouseofdesign.com>
2022-11-29 09:33:52 -0700

    final

--- scale-generator/src/lib.rs
+++ scale-generator/src/lib.rs
@@ -1,33 +1,56 @@
-// You should change this.
-//
-// Depending on your implementation, there are a variety of potential errors
-// which might occur. They aren't checked by the test suite in order to
-// allow the greatest freedom of implementation, but real libraries should
-// provide useful, descriptive errors so that downstream code can react
-// appropriately.
-//
-// One common idiom is to define an Error enum which wraps all potential
-// errors. Another common idiom is to use a helper type such as failure::Error
-// which does more or less the same thing but automatically.
-#[derive(Debug)]
-pub struct Error;
+// A, A#, B, C, C#, D, D#, E, F, F#, G, G#
+// A, Bb, B, C, Db, D, Eb, E, F, Gb, G, Ab
 
-pub struct Scale;
+#[derive(Debug)]
+pub enum Error {
+    BadInterval,
+    BadTonic,
+}
+
+pub struct Scale(Vec<String>);
+
+const SCALE_SHARPS: [&str; 12] = [
+    "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#",
+];
+
+const SCALE_FLATS: [&str; 12] = [
+    "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab",
+];
 
 impl Scale {
     pub fn new(tonic: &str, intervals: &str) -> Result<Scale, Error> {
-        unimplemented!(
-            "Construct a new scale with tonic {} and intervals {}",
-            tonic,
-            intervals
-        )
+        // find chromatic
+        dbg!(&tonic);
+        let chromatic_scale: Vec<&str> = match tonic {
+            "C" | "G" | "D" | "A" | "E" | "B" | "F#" | "e" | "b" | "f#" | "c#" | "g#" | "d#" | "a" => SCALE_SHARPS.to_vec(),
+            "F" | "Bb" | "Eb" | "Ab" | "Db" | "Gb" | "d" | "g" | "c" | "f" | "bb" | "eb" => SCALE_FLATS.to_vec(),
+            _ => return Err(Error::BadTonic),
+        };
+
+        // find index of starting note
+        let mut index = chromatic_scale.iter().position(|c| *c.to_uppercase()==tonic.to_uppercase()).unwrap_or_default();
+        
+        let mut _scale: Vec<String> = vec![chromatic_scale[index].to_string()];
+
+        // iterate through intervals and add notes to new scale
+        for step in intervals.chars() {
+            index += match step {
+                'm' => 1,
+                'M' => 2,
+                'A' => 3,
+                _ => return Err(Error::BadInterval),
+            };
+            _scale.push(chromatic_scale[index % chromatic_scale.len()].to_string());
+        }
+        Ok(Scale(_scale))
     }
 
     pub fn chromatic(tonic: &str) -> Result<Scale, Error> {
-        unimplemented!("Construct a new chromatic scale with tonic {}", tonic)
+        // A new chromatic is a new scale with all half steps
+        Self::new(tonic, "mmmmmmmmmmmm")
     }
 
     pub fn enumerate(&self) -> Vec<String> {
-        unimplemented!()
+        self.0.clone()
     }
 }
--- scale-generator/tests/scale-generator.rs
+++ scale-generator/tests/scale-generator.rs
@@ -41,7 +41,6 @@ fn test_chromatic_scale_with_sharps() {
 }
 
 #[test]
-#[ignore]
 /// Chromatic scale with flats
 fn test_chromatic_scale_with_flats() {
     process_chromatic_case(
@@ -57,7 +56,6 @@ fn test_chromatic_scale_with_flats() {
 // traversals of the scale.
 
 #[test]
-#[ignore]
 /// Simple major scale
 ///
 /// The simplest major scale, with no sharps or flats.
@@ -66,21 +64,18 @@ fn test_simple_major_scale() {
 }
 
 #[test]
-#[ignore]
 /// Major scale with sharps
 fn test_major_scale_with_sharps() {
     process_interval_case("G", "MMmMMMm", &["G", "A", "B", "C", "D", "E", "F#", "G"]);
 }
 
 #[test]
-#[ignore]
 /// Major scale with flats
 fn test_major_scale_with_flats() {
     process_interval_case("F", "MMmMMMm", &["F", "G", "A", "Bb", "C", "D", "E", "F"]);
 }
 
 #[test]
-#[ignore]
 /// Minor scale with sharps
 fn test_minor_scale_with_sharps() {
     process_interval_case(
@@ -91,7 +86,6 @@ fn test_minor_scale_with_sharps() {
 }
 
 #[test]
-#[ignore]
 /// Minor scale with flats
 fn test_minor_scale_with_flats() {
     process_interval_case(
@@ -102,14 +96,12 @@ fn test_minor_scale_with_flats() {
 }
 
 #[test]
-#[ignore]
 /// Dorian mode
 fn test_dorian_mode() {
     process_interval_case("d", "MmMMMmM", &["D", "E", "F", "G", "A", "B", "C", "D"]);
 }
 
 #[test]
-#[ignore]
 /// Mixolydian mode
 fn test_mixolydian_mode() {
     process_interval_case(
@@ -120,7 +112,6 @@ fn test_mixolydian_mode() {
 }
 
 #[test]
-#[ignore]
 /// Lydian mode
 fn test_lydian_mode() {
     process_interval_case(
@@ -131,14 +122,12 @@ fn test_lydian_mode() {
 }
 
 #[test]
-#[ignore]
 /// Phrygian mode
 fn test_phrygian_mode() {
     process_interval_case("e", "mMMMmMM", &["E", "F", "G", "A", "B", "C", "D", "E"]);
 }
 
 #[test]
-#[ignore]
 /// Locrian mode
 fn test_locrian_mode() {
     process_interval_case(
@@ -149,7 +138,6 @@ fn test_locrian_mode() {
 }
 
 #[test]
-#[ignore]
 /// Harmonic minor
 ///
 /// Note that this case introduces the augmented second interval (A)
@@ -158,7 +146,6 @@ fn test_harmonic_minor() {
 }
 
 #[test]
-#[ignore]
 /// Octatonic
 fn test_octatonic() {
     process_interval_case(
@@ -169,21 +156,18 @@ fn test_octatonic() {
 }
 
 #[test]
-#[ignore]
 /// Hexatonic
 fn test_hexatonic() {
     process_interval_case("Db", "MMMMMM", &["Db", "Eb", "F", "G", "A", "B", "Db"]);
 }
 
 #[test]
-#[ignore]
 /// Pentatonic
 fn test_pentatonic() {
     process_interval_case("A", "MMAMA", &["A", "B", "C#", "E", "F#", "A"]);
 }
 
 #[test]
-#[ignore]
 /// Enigmatic
 fn test_enigmatic() {
     process_interval_case(

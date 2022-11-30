dcc94dcf96856e16580db73a6af8c92379973d6c
Katherine <kstedman@thehouseofdesign.com>
2022-11-30 15:05:51 -0700

    cleaner, better, donzo

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,28 +1,11 @@
 pub fn nth(n: u32) -> u32 {
     let mut primes: Vec<u32> = vec![2, 3, 5];
-    let (mut a, mut k, mut p): (u32, u32, u32) = (5, 1, 0);
-
-    while primes.len()-1 < n as usize {
-        p = a + 2*k;
-        if !(is_prime(p, &primes)){
-            k += 1;
-        } else {
-            primes.push(p as u32);
-            a = p;
-            k = 1;
+    while primes.len() <= n as usize {
+        let mut next_prime = primes.last().unwrap() + 2;
+        while primes.iter().any(|x| next_prime % x == 0) {
+            next_prime += 2;
         }
+        primes.push(next_prime);
     }
     primes[n as usize]
-}
-
-pub fn is_prime(p: u32, primes: &[u32]) -> bool {
-    let s = ((p-1) as f64/2.0).round() as u32;
-    for i in primes.iter(){
-        if i > &s {
-            return true;
-        } else if p % i == 0 {
-            return false;
-        } else {};
-    }
-    true
 }

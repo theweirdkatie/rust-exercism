4dd6a0530abc71127e33021f1d893ef9dc22a5e0
Katherine <kstedman@thehouseofdesign.com>
2022-11-30 15:37:07 -0700

    done

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,22 @@
-pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+pub fn factors(mut n: u64) -> Vec<u64> {
+    let mut facts = vec![];
+    let mut p: u64 = 2;
+
+    if n<=1 {
+        facts
+    } else {
+        loop {
+            if n < p.pow(2) {
+                facts.push(n);
+                break;
+            } else if n % p == 0 {
+                facts.push(p);
+                n = n / p;
+                println!("{}", p);
+            } else {
+                p += 1;
+            }
+        }
+        facts
+    }
 }

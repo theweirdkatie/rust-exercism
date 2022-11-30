904262c6055af56c159249bcf9f066bb1f7e6a5d
Katherine <kstedman@thehouseofdesign.com>
2022-11-30 11:58:58 -0700

    easy peasy lemon squeezy

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,14 +1,17 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{}", n)
+    // Create an inclusive range from 1 to n
+    // Ranges are iterators, so sum each iteration
+    // Square the resulting number
+    (1..=n).sum::<u32>().pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{}", n)
+    // Create an inclusive range from 1 to n
+    // Ranges are iterators, so square each iteration
+    // Sum all the squares
+    (1..=n).map(|x| x.pow(2)).sum::<u32>()
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!(
-        "difference between square of sum of 1...{n} and sum of squares of 1...{n}",
-        n = n,
-    )
+    square_of_sum(n) - sum_of_squares(n)
 }

b49b4592bea0f37bf0485e3610b09a9980894681
Katherine <kstedman@thehouseofdesign.com>
2022-11-30 09:23:19 -0700

    re-download

--- minesweeper/src/lib.rs
+++ minesweeper/src/lib.rs
@@ -1,22 +1,3 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    minefield                                           // take original array
-        .iter()                                      // create iterator
-        .enumerate()           // enumerate
-        .map(|index, row| {                                     // map the index and row data
-            row.char_indices()                                  // where row data is indexed by character
-                .map(|col, val| match val {                     // and mapped to a column index and a single character value
-                    '*' => '*',                                 // where if the value is a bomb, it stays a bomb
-                    _ => count_bombs(minefield, index, col),    // but if it's anything else, count the bombs around that square
-                })
-                .collect::<String>()                            // collect these values as a Vec<String> for the row
-        })
-        .collect()                                              // collect all these values/rows into vector and return value
-}
-
-pub fn count_bombs(field: &[&str], row_num: usize, col_num:usize ) -> char {
-    let mut bombs = 0;
-    let range_start = if row_num > 0 {row_num-1} else {0};
-    let range_end = if row_num < (field.len() -1) {row_num + 2} else {field.len()};
-
-    bombs = field[(range_start..range_end)].iter().map(|&x| )
+    unimplemented!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{:#?}\n", minefield);
 }
--- minesweeper/tests/minesweeper.rs
+++ minesweeper/tests/minesweeper.rs
@@ -28,6 +28,7 @@ fn no_rows() {
 }
 
 #[test]
+#[ignore]
 fn no_columns() {
     #[rustfmt::skip]
     run_test(&[
@@ -36,6 +37,7 @@ fn no_columns() {
 }
 
 #[test]
+#[ignore]
 fn no_mines() {
     #[rustfmt::skip]
     run_test(&[
@@ -46,6 +48,7 @@ fn no_mines() {
 }
 
 #[test]
+#[ignore]
 fn board_with_only_mines() {
     #[rustfmt::skip]
     run_test(&[
@@ -56,6 +59,7 @@ fn board_with_only_mines() {
 }
 
 #[test]
+#[ignore]
 fn mine_surrounded_by_spaces() {
     #[rustfmt::skip]
     run_test(&[

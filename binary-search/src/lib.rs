bb7a456f7c23a494dd6e50506dc96ac84199d5c5
Katherine <kstedman@thehouseofdesign.com>
2022-12-01 10:39:20 -0700

    Initial try with indexing

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,7 +1,24 @@
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+    match array.len() {
+        0 => None,
+        1 => Some(0),
+        _ => {
+            let mut low = 0;
+            let mut high = array.len()-1;
+            while low <= high {
+                let mid = (low + high) / 2;
+                println!("{}, {}, {}", low, mid, high);
+                if array[mid] == key {
+                    return Some(mid);
+                } else if array[mid] < key {
+                    low = mid + 1;
+                } else if mid > 0 {
+                    high = mid - 1;
+                } else {
+                    break;
+                }
+            }
+            return None;
+        }
+    }
 }

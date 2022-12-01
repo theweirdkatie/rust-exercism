6f3d86d831d2d0761c9c29fab821371505fcd799
Katherine <kstedman@thehouseofdesign.com>
2022-12-01 10:46:18 -0700

    using ordering

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,19 +1,16 @@
+use std::cmp::Ordering;
+
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    if !array.contains(&key) {
-        return None;
-    } else {
-        let mut low = 0;
-        let mut high = array.len() - 1;
-        while low <= high {
-            let mid = (low + high) / 2;
-            if array[mid] == key {
-                return Some(mid);
-            } else if array[mid] < key {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+    let mut low = 0;
+    let mut high = array.len().checked_sub(1)?;
+
+    while low <= high {
+        let mid = low + (high - low) / 2;
+        match array[mid].cmp(&key) {
+            Ordering::Equal => return Some(mid),
+            Ordering::Less => low = mid + 1,
+            Ordering::Greater => high = mid.checked_sub(1)?,
         }
-        return None;
     }
+    None
 }

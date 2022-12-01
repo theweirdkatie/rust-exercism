ac87555b99e7decf910c9f11c09e838361e233f6
Katherine <kstedman@thehouseofdesign.com>
2022-12-01 13:52:38 -0700

    ugh, I looked at the example solution and it poisoned the well

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,6 +1,8 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+    match n {
+        0 => None,
+        1 => Some(0),
+        n if n % 2 == 0 => collatz(n / 2).map(|c| c + 1),
+        n => collatz(n.checked_mul(3)?.checked_add(1)?).map(|c| c + 1),
+    }
 }

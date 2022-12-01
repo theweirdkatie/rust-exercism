88058ddc6746a4fe13fea5c59e2b436649321bff
Katherine <kstedman@thehouseofdesign.com>
2022-12-01 10:42:24 -0700

    cleaned up

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,24 +1,19 @@
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    match array.len() {
-        0 => None,
-        1 => Some(0),
-        _ => {
-            let mut low = 0;
-            let mut high = array.len()-1;
-            while low <= high {
-                let mid = (low + high) / 2;
-                println!("{}, {}, {}", low, mid, high);
-                if array[mid] == key {
-                    return Some(mid);
-                } else if array[mid] < key {
-                    low = mid + 1;
-                } else if mid > 0 {
-                    high = mid - 1;
-                } else {
-                    break;
-                }
+    if !array.contains(&key) {
+        return None;
+    } else {
+        let mut low = 0;
+        let mut high = array.len() - 1;
+        while low <= high {
+            let mid = (low + high) / 2;
+            if array[mid] == key {
+                return Some(mid);
+            } else if array[mid] < key {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
-            return None;
         }
+        return None;
     }
 }

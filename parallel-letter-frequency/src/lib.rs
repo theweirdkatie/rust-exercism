d56c750fe515153b389e5fa42981904dfc34979e
Katherine <kstedman@thehouseofdesign.com>
2022-12-05 08:33:03 -0700

    Initial solution with arc and mutex

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -2,20 +2,23 @@ use std::{
     collections::HashMap,
     thread,
     sync::{Arc, Mutex},
+    ops::Deref,
 };
 
-pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
+pub fn frequency(input: &[&str], _worker_count: usize) -> HashMap<char, usize> {
     let mut workers = vec![];
-    let map: HashMap<char, usize> = HashMap::new();
-    let arc_map = Arc::new(Mutex::new(map));
+    let map = Arc::new(Mutex::new(HashMap::new()));
+    let cloned_input: Vec<String> = input.iter().map(|&x| x.clone().to_owned().to_lowercase()).collect();
 
-    for sentence in input {
-        let clone_arc_map = Arc::clone(&arc_map);
+    for sentence in cloned_input {
+        let clone_arc_map = Arc::clone(&map);
         let worker = thread::spawn(move || {
             let mut map = clone_arc_map.lock().unwrap();
             for c in sentence.chars(){
-                let count = map.entry(c).or_insert(0);
-                *count += 1;
+                if c.is_alphabetic() {
+                    let count = map.entry(c).or_insert(0);
+                    *count += 1;
+                }
             }
         });
         workers.push(worker);
@@ -25,5 +28,7 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
         worker.join().unwrap();
     }
 
-    map
+    let mut result: HashMap<char, usize> = HashMap::new();
+    result.extend(map.as_ref().lock().expect("dxfgh").deref());
+    result
 }

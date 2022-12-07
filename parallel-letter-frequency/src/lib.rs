847051a0a70c07ef003381f7d7724407ae269c9e
Katherine <kstedman@thehouseofdesign.com>
2022-12-07 15:09:49 -0700

    using chunks

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,32 +1,32 @@
 /*
-not great
-test bench_large_parallel   ... bench:  48,938,200 ns/iter (+/- 68,820,217)
-test bench_large_sequential ... bench:     640,000 ns/iter (+/- 23,729)
-test bench_small_parallel   ... bench:   1,627,537 ns/iter (+/- 104,106)
-test bench_small_sequential ... bench:      22,393 ns/iter (+/- 819)
-test bench_tiny_parallel    ... bench:     145,720 ns/iter (+/- 18,039)
-test bench_tiny_sequential  ... bench:         128 ns/iter (+/- 15)
+better
+test bench_large_parallel   ... bench:     614,383 ns/iter (+/- 70,580)
+test bench_large_sequential ... bench:     678,255 ns/iter (+/- 102,993)
+test bench_small_parallel   ... bench:     293,888 ns/iter (+/- 32,422)
+test bench_small_sequential ... bench:      23,781 ns/iter (+/- 443)
+test bench_tiny_parallel    ... bench:     160,651 ns/iter (+/- 19,066)
+test bench_tiny_sequential  ... bench:         133 ns/iter (+/- 2)
  */
 
 use std::{
     collections::HashMap,
     thread,
     sync::{Arc, Mutex},
-    ops::Deref,
 };
 
 pub fn frequency(input: &[&str], _worker_count: usize) -> HashMap<char, usize> {
     let mut workers = vec![];
     let map = Arc::new(Mutex::new(HashMap::new()));
-    let cloned_input: Vec<String> = input.iter().map(|&x| x.clone().to_owned().to_lowercase()).collect();
+    let chunks = input.chunks((input.len() / _worker_count).max(1));
 
-    for sentence in cloned_input {
-        let clone_arc_map = Arc::clone(&map);
+    for chunk in chunks {
+        let clone_arc_map = map.clone();
+        let string = chunk.join("");
         let worker = thread::spawn(move || {
             let mut map = clone_arc_map.lock().unwrap();
-            for c in sentence.chars(){
+            for c in string.chars() {
                 if c.is_alphabetic() {
-                    let count = map.entry(c).or_insert(0);
+                    let count = map.entry(c.to_ascii_lowercase()).or_insert(0);
                     *count += 1;
                 }
             }
@@ -38,7 +38,5 @@ pub fn frequency(input: &[&str], _worker_count: usize) -> HashMap<char, usize> {
         worker.join().unwrap();
     }
 
-    let mut result: HashMap<char, usize> = HashMap::new();
-    result.extend(map.as_ref().lock().expect("dxfgh").deref());
-    result
+    Arc::try_unwrap(map).unwrap().into_inner().unwrap()
 }

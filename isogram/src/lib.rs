8108b468232b5d30d3338e2fa87ba5d278239cc4
Katherine <kstedman@thehouseofdesign.com>
2022-12-08 17:14:56 -0700

    easy enough

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -1,3 +1,10 @@
+use std::collections::HashMap;
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {} an isogram?", candidate);
+    let mut map = HashMap::new();
+    for c in candidate.chars() {
+        if c.is_alphabetic() {
+            *map.entry(c.to_ascii_lowercase()).or_insert(0) += 1;
+        }
+    }
+    map.len() == 0 || !map.into_values().any(|x| x > 1)
 }

085ad1579741291e32f018e278695a8a5ad9406c
Katherine <kstedman@thehouseofdesign.com>
2022-12-09 12:01:15 -0700

    easy peasy

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,9 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    match list.first() {
+        None => String::new(),
+        Some(word) => list
+            .windows(2)
+            .map(|_str| format!("For want of a {} the {} was lost.\n", _str[0], _str[1]))
+            .collect::<String>() + &format!("And all for the want of a {}.", word)
+    }
 }

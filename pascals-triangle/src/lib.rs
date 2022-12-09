7431d1f12e00edcb333469da61656a30b44ac8da
Katherine <kstedman@thehouseofdesign.com>
2022-12-09 14:57:33 -0700

    yeah

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -1,11 +1,25 @@
-pub struct PascalsTriangle;
+pub struct PascalsTriangle {
+    row_count: u32,
+}
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        unimplemented!("create Pascal's triangle with {} rows", row_count);
+        Self { row_count }
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        unimplemented!();
+        println!("Row count: {}", self.row_count);
+        (0..self.row_count).map(|row| PascalsTriangle::generate_row(row)).collect()
+    }
+
+    pub fn generate_row(row_num: u32) -> Vec<u32> {
+        let mut result = vec![1];
+        for col in 1..=row_num {
+            println!("Here!");
+            if let Some(last) = result.last() {
+                result.push((last*(row_num+1 - col))/col);
+            }
+        }
+        result
     }
 }

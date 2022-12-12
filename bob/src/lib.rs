27d6038d2ac94510b5e1ce2f28d58a36e426eade
Katherine <kstedman@thehouseofdesign.com>
2022-12-12 14:59:19 -0700

    done

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    let trimmed = message.trim();
+
+    if trimmed.is_empty() {
+        return "Fine. Be that way!";
+    }
+
+    let is_yelling = trimmed.to_uppercase() == trimmed && trimmed.chars().any(|c| c.is_alphabetic());
+    let is_question = trimmed.ends_with('?');
+
+    match (is_yelling, is_question) {
+        (true, true) => "Calm down, I know what I'm doing!",
+        (true, false) => "Whoa, chill out!",
+        (false, true) => "Sure.",
+        (false, false) => "Whatever.",
+    }
 }

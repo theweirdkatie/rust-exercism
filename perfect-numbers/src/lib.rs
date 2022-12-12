4bfa040692e270c773ce2c49906e82731f60cd6a
Katherine <kstedman@thehouseofdesign.com>
2022-12-12 15:12:02 -0700

    simple enough

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 #[derive(Debug, PartialEq, Eq)]
 pub enum Classification {
     Abundant,
@@ -6,5 +7,11 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {}", num);
+    if num == 0 { return None; }
+    let sum = (1..num).fold(0, |acc, n| if num % n == 0 {acc + n} else {acc + 0});
+    match sum.cmp(&num) {
+        Ordering::Equal => Some(Classification::Perfect),
+        Ordering::Greater => Some(Classification::Abundant),
+        Ordering::Less => Some(Classification::Deficient),
+    }
 }

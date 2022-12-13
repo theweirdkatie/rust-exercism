de133d07515f52edcf298e08b83718c8f516477e
Katherine <kstedman@thehouseofdesign.com>
2022-12-13 12:47:18 -0700

    cleaner decode

--- run-length-encoding/src/lib.rs
+++ run-length-encoding/src/lib.rs
@@ -4,7 +4,7 @@ pub fn encode(source: &str) -> String {
     let mut encoded_string = String::new();
     let mut count: u32 = 1;
     for (i, current_char) in source.chars().enumerate() {
-        if let Some(next_char) = source.chars().nth(i+1) {
+        if let Some(next_char) = source.chars().nth(i + 1) {
             if current_char == next_char {
                 count += 1;
             } else {
@@ -20,42 +20,15 @@ pub fn encode(source: &str) -> String {
             }
             encoded_string.push(current_char);
         }
-
     }
     encoded_string
 }
 
 pub fn decode(source: &str) -> String {
-    let organized_vec = organize_vec(source);
-    let mut decoded_string = String::new();
-    for entry in organized_vec.iter() {
-        if entry.len() > 1 {
-            if let Ok(count) = entry[..(entry.len()-1)].parse() {
-                for _ in 0..count {
-                    decoded_string.push_str(&entry[entry.len()-1..]);
-                }
-            }
-        } else {
-            decoded_string.push_str(&entry[..entry.len()]);
-        }
-    }
-    decoded_string
-}
-
-pub fn organize_vec(source: &str) -> Vec<String> {
-    let mut iter = source.chars();
-    let mut result = vec![];
-    let mut count = 1;
-    result.push("".to_string());
-    while let Some(current_char) = iter.next() {
-        let index = result.len()-1;
-        if current_char.is_ascii_digit() {
-            count += 1;
-        } else {
-            count = 1;
-        }
-        result[index].push(current_char);
-        if count == 1 {result.push("".to_string());}
-    }
-    result
+    source
+        .split(|c: char| !c.is_digit(10))
+        .map(|n_str| n_str.parse::<usize>().unwrap_or(1))
+        .zip(source.matches(|c: char| !c.is_digit(10)))
+        .map(|(num, c)| c.repeat(num))
+        .collect()
 }

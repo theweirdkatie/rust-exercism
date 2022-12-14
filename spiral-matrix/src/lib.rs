5196492e1b34d779d2556cfae29364a9c264122a
Katherine <kstedman@thehouseofdesign.com>
2022-12-14 13:03:30 -0700

    no likey

--- spiral-matrix/src/lib.rs
+++ spiral-matrix/src/lib.rs
@@ -1,6 +1,41 @@
 pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
-    unimplemented!(
-        "Function that returns the spiral matrix of square size {}",
-        size
-    );
+    let mut result: Vec<Vec<u32>> = (0..size).map(|_| (0..size).map(|_| 0).collect()).collect();
+    let (mut row, mut col): (i32, i32) = (0, 0);
+    let (mut d_row, mut d_col): (i32, i32) = (0, 1);
+
+    for count in 1..=(size*size) {
+        result[row as usize][col as usize] = count;
+        row += d_row;
+        col += d_col;
+
+        // if col # is =size or more OR the col direction is positive and the next spot has been filled
+        // Col movement freezes (=0), row direction becomes positive, col backs off and row increases
+        // east to south
+        if col >= size as i32 || (d_col == 1 && result[row as usize][col as usize] != 0) {
+            (d_row, d_col) = (1, 0);
+            col -= 1; row += 1;
+        
+        // if row # is =size or more OR the row direction is positive and the next spot has been filled
+        // Row movement freezes (=0), col direction becomes negative, row backs off and col backs off
+        // south to west
+        } else if row >= size as i32 || (d_row == 1 && result[row as usize][col as usize] != 0) {
+            (d_row, d_col) = (0, -1);
+            row -= 1; col -= 1;
+        
+        // if col # is < 0 OR the col direction is negative and the next spot has been filled
+        // Col movement freezes (=0), row direction becomes negative, col corrects and row decreases
+        // west to north
+        } else if col < 0 || (d_col == -1 && result[row as usize][col as usize] != 0) {
+            (d_row, d_col) = (-1, 0);
+            col += 1; row -= 1;
+        
+        // if row # is < 0 OR the row direction is negative and the next spot has been filled
+        // Row movement freezes (=0), col direction becomes positive, row corrects and col increases
+        // north to east
+        } else if row < 0 || (d_row == -1 && result[row as usize][col as usize] != 0) {
+            (d_row, d_col) = (0, 1);
+            row += 1; col += 1;
+        }
+    }
+    result
 }
--- spiral-matrix/tests/spiral-matrix.rs
+++ spiral-matrix/tests/spiral-matrix.rs
@@ -7,20 +7,17 @@ fn empty_spiral() {
 }
 
 #[test]
-#[ignore]
 fn size_one_spiral() {
     let expected: Vec<Vec<u32>> = vec![vec![1]];
     assert_eq!(spiral_matrix(1), expected);
 }
 #[test]
-#[ignore]
 fn size_two_spiral() {
     let expected: Vec<Vec<u32>> = vec![vec![1, 2], vec![4, 3]];
     assert_eq!(spiral_matrix(2), expected);
 }
 
 #[test]
-#[ignore]
 fn size_three_spiral() {
     #[rustfmt::skip]
     let expected: Vec<Vec<u32>> = vec![
@@ -31,7 +28,6 @@ fn size_three_spiral() {
     assert_eq!(spiral_matrix(3), expected);
 }
 #[test]
-#[ignore]
 fn size_four_spiral() {
     let expected: Vec<Vec<u32>> = vec![
         vec![1, 2, 3, 4],
@@ -42,7 +38,6 @@ fn size_four_spiral() {
     assert_eq!(spiral_matrix(4), expected);
 }
 #[test]
-#[ignore]
 fn size_five_spiral() {
     let expected: Vec<Vec<u32>> = vec![
         vec![1, 2, 3, 4, 5],

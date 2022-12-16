6af02af0a37701e52a0563f61458485b52154ee9
Katherine <kstedman@thehouseofdesign.com>
2022-12-16 11:28:24 -0700

    basic u64 solution

--- triangle/src/lib.rs
+++ triangle/src/lib.rs
@@ -1,19 +1,25 @@
-pub struct Triangle;
+pub struct Triangle {
+    sides: [u64; 3], 
+}
 
 impl Triangle {
     pub fn build(sides: [u64; 3]) -> Option<Triangle> {
-        unimplemented!("Construct new Triangle from following sides: {:?}. Return None if the sides are invalid.", sides);
+        if sides.iter().all(|&x| x>0 ) && sides[0]+sides[1] >= sides[2] && sides[1]+sides[2] >= sides[0]{
+            return Some(Triangle{sides});
+        } else {
+            return None;
+        }
     }
 
     pub fn is_equilateral(&self) -> bool {
-        unimplemented!("Determine if the Triangle is equilateral.");
+        self.sides.as_slice().windows(2).all(|x| x[0] == x[1])
     }
 
     pub fn is_scalene(&self) -> bool {
-        unimplemented!("Determine if the Triangle is scalene.");
+        self.sides[0] != self.sides[1] && self.sides[0] != self.sides[2] && self.sides[1] != self.sides[2]
     }
 
     pub fn is_isosceles(&self) -> bool {
-        unimplemented!("Determine if the Triangle is isosceles.");
+        !self.is_equilateral() && !self.is_scalene()
     }
 }

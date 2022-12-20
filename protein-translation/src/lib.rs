c17438dd1a68c45879eac76fd8b1e74b09f355a6
Katherine <kstedman@thehouseofdesign.com>
2022-12-20 11:00:51 -0700

    simple enough

--- protein-translation/src/lib.rs
+++ protein-translation/src/lib.rs
@@ -1,25 +1,24 @@
+use std::collections::HashMap;
+#[derive(Debug)]
 pub struct CodonsInfo<'a> {
-    // We fake using 'a here, so the compiler does not complain that
-    // "parameter `'a` is never used". Delete when no longer needed.
-    phantom: std::marker::PhantomData<&'a ()>,
+    codons: HashMap<&'a str, &'a str>,
 }
 
 impl<'a> CodonsInfo<'a> {
     pub fn name_for(&self, codon: &str) -> Option<&'a str> {
-        unimplemented!(
-            "Return the protein name for a '{}' codon or None, if codon string is invalid",
-            codon
-        );
+        self.codons.get(codon).cloned()
     }
 
     pub fn of_rna(&self, rna: &str) -> Option<Vec<&'a str>> {
-        unimplemented!("Return a list of protein names that correspond to the '{}' RNA string or None if the RNA string is invalid", rna);
+        rna.chars().collect::<Vec<char>>()
+            .chunks(3)
+            .map(|ch| self.name_for(ch.iter().collect::<String>().as_ref()))
+            .take_while(|&name| name != Some("stop codon"))
+            .collect()
     }
 }
 
 pub fn parse<'a>(pairs: Vec<(&'a str, &'a str)>) -> CodonsInfo<'a> {
-    unimplemented!(
-        "Construct a new CodonsInfo struct from given pairs: {:?}",
-        pairs
-    );
+    let codons = pairs.into_iter().collect();
+    CodonsInfo { codons }
 }

a40d126030e879e618f0af2902b2a44563c832c3
Katherine <kstedman@thehouseofdesign.com>
2022-12-20 09:51:38 -0700

    6 lines -.-

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,23 +1,7 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    if upper_bound < 2 {
-        return vec![];
+    let mut numbers: Vec<u64> = (2..=upper_bound).collect();
+    for num in 2..=(upper_bound as f64).sqrt() as u64 {
+        numbers.retain(|&x| x % num != 0 || x == num);
     }
-    let mut is_prime: Vec<bool> = (2..=upper_bound).map(|_| true).collect();
-    for n in 2..=(upper_bound as f64).sqrt() as usize {
-        if is_prime[n - 2] {
-            let mut i = 0;
-            let mut num: usize = 2 * n;
-            while num <= upper_bound as usize {
-                is_prime[num - 2] = false;
-                i += 1;
-                num = 2 * n + i * n;
-            }
-        }
-    }
-    is_prime
-        .iter()
-        .enumerate()
-        .map(|(idx, &val)| if val { (idx + 2) as u64 } else { 0_u64 })
-        .filter(|&x| x > 0)
-        .collect::<Vec<u64>>()
+    numbers
 }

55d4d71a3205a606e4e82a0857d0f4767cc4e6e6
Katherine <kstedman@thehouseofdesign.com>
2022-12-20 09:44:04 -0700

    my way

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,3 +1,23 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    unimplemented!("Construct a vector of all primes up to {}", upper_bound);
+    if upper_bound < 2 {
+        return vec![];
+    }
+    let mut is_prime: Vec<bool> = (2..=upper_bound).map(|_| true).collect();
+    for n in 2..=(upper_bound as f64).sqrt() as usize {
+        if is_prime[n - 2] {
+            let mut i = 0;
+            let mut num: usize = 2 * n;
+            while num <= upper_bound as usize {
+                is_prime[num - 2] = false;
+                i += 1;
+                num = 2 * n + i * n;
+            }
+        }
+    }
+    is_prime
+        .iter()
+        .enumerate()
+        .map(|(idx, &val)| if val { (idx + 2) as u64 } else { 0_u64 })
+        .filter(|&x| x > 0)
+        .collect::<Vec<u64>>()
 }

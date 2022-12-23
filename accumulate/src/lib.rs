b6c49202c1ba8f0ada49fd11fc72ef766ef8c8e7
Katherine <kstedman@thehouseofdesign.com>
2022-12-22 17:01:47 -0700

    all by myself

--- accumulate/src/lib.rs
+++ accumulate/src/lib.rs
@@ -1,4 +1,9 @@
-/// What should the type of _function be?
-pub fn map(input: Vec<i32>, _function: ???) -> Vec<i32> {
-    unimplemented!("Transform input vector {:?} using passed function", input);
+pub fn map<F, T, U>(input: Vec<T>, mut _function: F) -> Vec<U> 
+    where F: FnMut(T) -> U
+{
+    let mut result = vec![];
+    for i in input {
+        result.push(_function(i));
+    }
+    result
 }

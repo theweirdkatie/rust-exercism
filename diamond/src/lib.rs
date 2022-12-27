97787e5e92a5861e3f42bc9032db4fbe5f9dbb8f
kstedman <kstedman.engr@gmail.com>
2022-12-26 20:37:17 -0800

    my way

--- diamond/src/lib.rs
+++ diamond/src/lib.rs
@@ -1,6 +1,22 @@
 pub fn get_diamond(c: char) -> Vec<String> {
-    unimplemented!(
-        "Return the vector of strings which represent the diamond with particular char {}",
-        c
-    );
+    let diff = (u32::from(c.to_ascii_uppercase()) - u32::from('A')) as usize;
+    let mut result = vec![];
+    for (i, letter) in ('A'..=c.to_ascii_uppercase()).enumerate() {
+        result.push(letter.to_string());
+        if i > 0 {
+            for _ in 0..(i*2 - 1) {
+                result[i].push(' ');
+            }
+            result[i].push(letter);
+        }
+        for _ in i..diff {
+            result[i] = format!(" {} ", result[i]);
+        }
+    }
+    if result.len() > 1 {
+        for i in (1..result.len()).rev() {
+            result.push(result[i-1].clone());
+        }
+    }
+    result
 }

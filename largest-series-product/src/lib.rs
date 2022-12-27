5f46cc9172fa462f2ebdc5fcfb2dbfa2187dc9e3
kstedman <kstedman.engr@gmail.com>
2022-12-26 21:05:52 -0800

    done

--- largest-series-product/src/lib.rs
+++ largest-series-product/src/lib.rs
@@ -5,9 +5,20 @@ pub enum Error {
 }
 
 pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
-    unimplemented!(
-        "largest series product of a span of {} digits in {}",
-        span,
-        string_digits
-    );
+    if span == 0 {
+        return Ok(1);
+    } else if string_digits.len() < span {
+        return Err(Error::SpanTooLong);
+    }
+    let mut largest_product: u64 = 0;
+    let mut digits = vec![];
+    for ch in string_digits.chars() {
+        if let Some(num) = ch.to_digit(10) {
+            digits.push(num as u64);
+        } else {
+            return Err(Error::InvalidDigit(ch));
+        }
+    }
+    digits.windows(span).for_each(|sp| if sp.iter().product::<u64>() > largest_product { largest_product = sp.iter().product() });
+    Ok(largest_product)
 }

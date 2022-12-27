1ac5fb77bfb279a690c9788146f4c844678ef6ca
kstedman <kstedman.engr@gmail.com>
2022-12-26 20:38:13 -0800

    -.-

--- diamond/src/lib.rs
+++ diamond/src/lib.rs
@@ -1,22 +1,16 @@
 pub fn get_diamond(c: char) -> Vec<String> {
-    let diff = (u32::from(c.to_ascii_uppercase()) - u32::from('A')) as usize;
-    let mut result = vec![];
-    for (i, letter) in ('A'..=c.to_ascii_uppercase()).enumerate() {
-        result.push(letter.to_string());
-        if i > 0 {
-            for _ in 0..(i*2 - 1) {
-                result[i].push(' ');
-            }
-            result[i].push(letter);
-        }
-        for _ in i..diff {
-            result[i] = format!(" {} ", result[i]);
-        }
-    }
-    if result.len() > 1 {
-        for i in (1..result.len()).rev() {
-            result.push(result[i-1].clone());
-        }
-    }
-    result
+    let n = (c as u8 - b'A') as i8;
+    (-n..=n)
+        .map(|row| {
+            (-n..=n)
+                .map(|col| {
+                    if row.abs() + col.abs() == n {
+                        (col.abs() as u8 + b'A') as char
+                    } else {
+                        ' '
+                    }
+                })
+                .collect()
+        })
+        .collect()
 }

f16beb5ba6d00eacf3a2ea6eb164575d22f24e83
Katherine <kstedman@thehouseofdesign.com>
2022-12-28 11:59:52 -0700

    done

--- phone-number/src/lib.rs
+++ phone-number/src/lib.rs
@@ -1,6 +1,18 @@
 pub fn number(user_number: &str) -> Option<String> {
-    unimplemented!(
-        "Given the number entered by user '{}', convert it into SMS-friendly format. If the entered number is not a valid NANP number, return None.",
-        user_number
-    );
+    let number = user_number.chars().filter(|ch| ch.is_digit(10)).collect::<String>();
+    match number.len() {
+        11 => {
+            if &number[0..1] == "1"  && is_valid(&number[1..]) {
+                return Some(number[1..].to_string());
+            } else {
+                return None;
+            }
+        }
+        10 => if is_valid(&number) { Some(number) } else { None },
+        _ => None,
+    }
+}
+
+pub fn is_valid(phone_number: &str) -> bool {
+    ("2"..="9").contains(&&phone_number[0..1]) && ("2"..="9").contains(&&phone_number[3..4])
 }

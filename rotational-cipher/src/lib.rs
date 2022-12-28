89c8ef6a7b6cac5ca10c74405a6a489b43422a86
Katherine <kstedman@thehouseofdesign.com>
2022-12-28 16:03:44 -0700

    my solution - readable

--- rotational-cipher/src/lib.rs
+++ rotational-cipher/src/lib.rs
@@ -1,7 +1,20 @@
 pub fn rotate(input: &str, key: i8) -> String {
-    unimplemented!(
-        "How would input text '{}' transform when every letter is shifted using key '{}'?",
-        input,
-        key
-    );
+    let mut cipher_alphabet = ('a'..='z').collect::<Vec<char>>();
+    if key > 0 {
+        cipher_alphabet.rotate_left(key.abs() as usize);
+    } else {
+        cipher_alphabet.rotate_right(key.abs() as usize);
+    }
+    input
+        .chars()
+        .map(|ch| {
+            if let Some(i) = ('a'..='z').position(|c| c == ch) {
+                cipher_alphabet[i]
+            } else if let Some(j) = ('A'..='Z').position(|c| c == ch) {
+                cipher_alphabet[j].to_ascii_uppercase()
+            } else {
+                ch
+            }
+        })
+        .collect::<String>()
 }

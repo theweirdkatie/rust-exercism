77e795c74fdafe53e0073bd250323b1d66ae7b89
Katherine <kstedman@thehouseofdesign.com>
2022-12-28 12:48:17 -0700

    done

--- rail-fence-cipher/src/lib.rs
+++ rail-fence-cipher/src/lib.rs
@@ -1,15 +1,33 @@
-pub struct RailFence;
+pub struct RailFence {
+    rails: u32,
+}
 
 impl RailFence {
     pub fn new(rails: u32) -> RailFence {
-        unimplemented!("Construct a new fence with {} rails", rails)
+        Self { rails }
     }
 
     pub fn encode(&self, text: &str) -> String {
-        unimplemented!("Encode this text: {}", text)
+        let mut code = vec![String::new(); self.rails as usize];
+        let zig_zag = (0..self.rails - 1).chain((1..self.rails).rev()).cycle();
+        for (ch, i) in text.chars().zip(zig_zag) {
+            code[i as usize].push(ch);
+        }
+        code.join("")
     }
 
     pub fn decode(&self, cipher: &str) -> String {
-        unimplemented!("Decode this ciphertext: {}", cipher)
+        let zig_zag = (0..self.rails - 1).chain((1..self.rails).rev()).cycle();
+        let mut indexes: Vec<_> = zig_zag.zip(1..).take(cipher.len()).collect();
+        indexes.sort();
+
+        let mut char_with_index: Vec<_> = cipher
+            .chars()
+            .zip(indexes)
+            .map(|(c, (_, i))| (i, c))
+            .collect();
+
+        char_with_index.sort();
+        char_with_index.iter().map(|(_, c)| c).collect()
     }
 }

40a7c08a20979be28ae77c421c3ebeed17a4dd8d
Katherine <kstedman@thehouseofdesign.com>
2022-12-28 11:15:26 -0700

    code cleanup

--- luhn-trait/src/lib.rs
+++ luhn-trait/src/lib.rs
@@ -2,11 +2,6 @@ pub trait Luhn {
     fn valid_luhn(&self) -> bool;
 }
 
-/// Here is the example of how to implement custom Luhn trait
-/// for the &str type. Naturally, you can implement this trait
-/// by hand for the every other type presented in the test suite,
-/// but your solution will fail if a new type is presented.
-/// Perhaps there exists a better solution for this problem?
 impl<T: ToString> Luhn for T {
     fn valid_luhn(&self) -> bool {
         let luhn = self.to_string();

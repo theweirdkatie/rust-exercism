d2b5c957cf38b9945154f8d43d4b4ef180c65aff
Katherine <kstedman@thehouseofdesign.com>
2022-12-30 09:36:21 -0700

    done

--- word-count/src/lib.rs
+++ word-count/src/lib.rs
@@ -1,6 +1,16 @@
 use std::collections::HashMap;
 
-/// Count occurrences of words.
 pub fn word_count(words: &str) -> HashMap<String, u32> {
-    unimplemented!("Count of occurrences of words in {:?}", words);
+    let mut map = HashMap::new();
+    for word in words.split(['\n', ' ', ',']) {
+        let len = word.len();
+        let ent = word.char_indices()
+            .filter(|(i, c)| c.is_ascii_alphanumeric() || (*c == '\'' && *i != 0 && i + 1 != len))
+            .map(|(_, x)| x.to_ascii_lowercase())
+            .collect::<String>();
+        if ent.len() > 0 {
+            *map.entry(ent).or_insert(0) += 1;
+        }
+    }
+    map
 }
